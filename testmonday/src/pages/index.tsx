846f1b958b48c9285ca92f3c54a5467ef670dc72
Songqi(Steven) Pu <45271050+pusongqi@users.noreply.github.com>
2023-05-15 09:48:24 -0700

    Add files via upload

---
+++ testmonday/src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import LoginForm from "../../components/LoginForm";
+import Timer from "../../components/Timer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta
+          name="this is not important"
+          content="The key to this website is row 5 and column 4."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div>
+          <Timer />
+          <LoginForm />
+        </div>
+      </main>
+    </>
+  );
+}
