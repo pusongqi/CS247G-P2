c77ba56277ffe29aa8718c5cba94a0e86293d402
Steven Pu <pusongqi@gmail.com>
2023-05-18 10:28:16 -0700

    thursday checkpoint 2

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -1,27 +1,38 @@
 import styles from "@/styles/LoginForm.module.css";
-import { useState } from "react";
+import { use, useState } from "react";
+import Countdown from "react-countdown";
+import { useEffect } from "react";
 
 function LoginForm(): JSX.Element {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  let res = ["3", "95", "69", "7"];
+  const [tries, setTries] = useState(res);
+  const [numberOfTries, setNumberOfTries] = useState(5);
 
-  const handleChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.currentTarget.textContent === tries[0]) {
+      setTries(tries.slice(1));
+    } else {
+      setTries(res);
+      setNumberOfTries(numberOfTries - 1);
+    }
   };
 
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleLogin = () => {
-    // some logic to submit to backend
-  };
+  useEffect(() => {
+    if (tries.length === 0) {
+      alert("From Satoshi: you are worthy of my fortune, 9895");
+    }
+    if (numberOfTries === 0) {
+      alert("From Satoshi: you are not worthy of my fortune.");
+    }
+  }, [tries]);
 
   function renderInputs() {
     const inputs = [];
     for (let i = 0; i < 104; i++) {
       inputs.push(
-        <div key={i} className={styles.card}>
+        <div key={i} className={styles.card} onClick={handleClick}>
           {i}
         </div>,
       );
@@ -31,7 +42,10 @@ function LoginForm(): JSX.Element {
 
   return (
     <div>
-      <div className={styles.title}>Inspect Today, Perfect Tomorrow.</div>
+      <div className={styles.warning}>You have {numberOfTries} tries left</div>
+      <div className={styles.title}>
+        "Inspect Today, Perfect Tomorrow." - Me
+      </div>
       <div>
         <div className={styles.grid}>{renderInputs()}</div>
       </div>
